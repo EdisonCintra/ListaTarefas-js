a110a1811ff02f7a9ac95e78fc1dc2c3c5f96d08
Edison Cintra de Oliveira Filho <edisoncintra6@gmail.com>
2024-03-14 19:11:31 -0300

    Mudanças de Layout

--- script.js
+++ script.js
@@ -1,90 +1,63 @@
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
+}
 
-    let banco = [
-
-    ];
-
-    const getBanco = () => JSON.parse(localStorage.getItem("todoList")) ?? []
-    const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco))
-
-    const criarItem = (tarefa, status, indice) => {
+//revisar ID!!!
+function addTarefa() {
+    const input = document.getElementById("valorTarefa")
+    let valorInput = input.value;
+    if (valorInput !== "") {
         const item = document.createElement('label');
-        item.className="border-2 border border-dark d-flex align-items-center gap-4 p-2 ";
+        item.setAttribute("id", "item")
+        item.className="border-2 border text-center justify-content-center border-dark d-flex align-items-center gap-4 p-2 ";
         item.innerHTML = `
-        <input class="form-check-input p-3 mb-1" type="checkbox" ${status} data-indice=${indice}>
-        <div class="text-break">${tarefa}</div>
-        <button type="button" class="btn-close" data-indice=${indice} aria-label="Close"></button>
-    `;
-
+            <input class="form-check-input p-3 mb-1 mt-3" type="checkbox">
+            <div class="text-break">${valorInput}</div>
+            <button type="button" class="btn-close" aria-label="Close" onclick="deletarTarefa()"></button>
+        `;
         document.getElementById('todoList').appendChild(item);
+        input.value = "";
+        input.focus();
     }
-
-
-    const limparTarefas = () => {
-        const todoList = document.getElementById('todoList')
-
-        while (todoList.firstChild){
-            todoList.removeChild(todoList.lastChild)
-        }
-    }
-    const atualizarTela = () => {
-        limparTarefas()
-        const banco = getBanco()
-        banco.forEach( (item,indice ) => criarItem(item.tarefa, item.status, indice))
-    }
-
-
-    const inserirItem = (evento) => {
-        const tecla = evento.key;
-        const valor = evento.target.value
-        if (tecla === "Enter"){
-            const banco = getBanco()
-            banco.push ({'tarefa' : valor, 'status' : ''})
-            setBanco(banco)
-            atualizarTela();
-            evento.target.value = ''
-        }
-    }
-
-    const removerItem = (indice) => {
-        const banco = getBanco()
-        banco.splice(indice, 1)
-        setBanco(banco)
-        atualizarTela()
-    }
-
-    const atualizarItem = (indice) =>{
-        const banco = getBanco()
-        banco[indice].status = banco[indice].status === '' ? 'checked' :  ''
-        setBanco(banco)
-        atualizarTela()
-
-    }
-
-    const clickItem = (evento) => {
-        const elemento = evento.target
-
-        if(elemento.type === "button"){
-            const indice = elemento.dataset.indice
-            removerItem(indice)
-        }else if(elemento.type === "checkbox"){
-            const indice = elemento.dataset.indice
-            atualizarItem(indice)
-        }
-    }
-
-    document.getElementById("valorTarefa").addEventListener('keypress', inserirItem)
-    document.getElementById("todoList").addEventListener('click', clickItem)
-
-
-
-    atualizarTela()
-
-
 }
 
 
+//revisar ID!!!
+function addLista() {
+    const div = document.createElement("div")
+    const input = document.getElementById("valorLista")
+    div.style.maxWidth = "600px";
+    div.innerHTML = `<div class="container border border-primary p-2 fw-bolder fs-1  justify-content-start
+    mb-3 col-12 gap-2 d-flex flex-column" id='todoList'>
+    <p class="mx-4 text-center">${input.value}</p>
+    <input type="text" class="form-control" id="valorTarefa" aria-label="First name">
+    <div class="d-flex flex-row justify-content-center d-grid gap-3">
+        <button class="btn btn-primary col-5" onclick="addTarefa()">Adicionar Tarefa</button>
+        <button class="btn btn-danger col-5" id="dell" onclick="deletarLista()">Deletar Lista</button>
+    </div>
+    </div>`
+    document.getElementById("containerLista").appendChild(div)
+    input.value = "";
+    input.focus();
+}
 
 
+//revisar!!!
+function deletarLista() {
+    let tarefa = document.getElementById("todoList");
+    tarefa.remove();
+}
+
+//revisar!!!
+function deletarTarefa(botao) {
+    let tarefa = document.getElementById("item")
+    tarefa.remove();
+}
+
+function deletarTodasListas(){
+    let listas = document.getElementById("containerLista");
+    while (listas.firstChild) {
+        listas.removeChild(listas.firstChild);
+    }
+}
