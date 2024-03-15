e91d030fffd7aba43124c1bf0307bdc1dab5fe67
Edison Cintra de Oliveira Filho <edisoncintra6@gmail.com>
2024-03-15 19:14:28 -0300

    Mudança no script.js, próxima alteração é usar o localstorage para armazenar.

--- script.js
+++ script.js
@@ -2,58 +2,103 @@ window.addEventListener("DOMContentLoaded", main);
 
 function main() {
 }
+function addTarefa(lista, inputValorTarefa) {
+    const valorInput = inputValorTarefa.value.trim();
+    const divValidation = lista.querySelector('.invalid-feedback');
 
-//revisar ID!!!
-function addTarefa() {
-    const input = document.getElementById("valorTarefa")
-    let valorInput = input.value;
-    if (valorInput !== "") {
-        const item = document.createElement('label');
-        item.setAttribute("id", "item")
-        item.className="border-2 border text-center justify-content-center border-dark d-flex align-items-center gap-4 p-2 ";
-        item.innerHTML = `
-            <input class="form-check-input p-3 mb-1 mt-3" type="checkbox">
-            <div class="text-break">${valorInput}</div>
-            <button type="button" class="btn-close" aria-label="Close" onclick="deletarTarefa()"></button>
-        `;
-        document.getElementById('todoList').appendChild(item);
-        input.value = "";
-        input.focus();
+    if (valorInput) {
+        const labelTarefa = document.createElement("label");
+        const input = document.createElement("input");
+        const p = document.createElement("p");
+        const buttonDel = document.createElement("button");
+
+        buttonDel.className = "btn-close";
+        labelTarefa.className = "border-2 border border-dark d-flex flex-row justify-content-center justify-content-evenly  align-items-center p-2 ";
+        input.className = "form-check-input p-3";
+        input.setAttribute("type", "checkbox");
+        p.className = "text-break fs-1 mt-2";
+        p.innerHTML = valorInput;
+
+        labelTarefa.append(input);
+        labelTarefa.append(p);
+        labelTarefa.append(buttonDel);
+
+        buttonDel.onclick = () => {
+            labelTarefa.remove();
+        };
+
+        lista.append(labelTarefa);
+        inputValorTarefa.value = "";
+
+        if (divValidation) {
+            divValidation.remove();
+        }
+
+    } else {
+        if (!divValidation) {
+            const divValidation = document.createElement("div");
+            divValidation.innerText = "Por favor, insira um valor para a tarefa.";
+            divValidation.style.display = "block";
+            divValidation.className = "invalid-feedback text-center";
+            lista.append(divValidation);
+        }
     }
 }
 
 
-//revisar ID!!!
 function addLista() {
-    const div = document.createElement("div")
-    const input = document.getElementById("valorLista")
-    div.style.maxWidth = "600px";
-    div.innerHTML = `<div class="container border border-primary p-2 fw-bolder fs-1  justify-content-start
-    mb-3 col-12 gap-2 d-flex flex-column" id='todoList'>
-    <p class="mx-4 text-center">${input.value}</p>
-    <input type="text" class="form-control" id="valorTarefa" aria-label="First name">
-    <div class="d-flex flex-row justify-content-center d-grid gap-3">
-        <button class="btn btn-primary col-5" onclick="addTarefa()">Adicionar Tarefa</button>
-        <button class="btn btn-danger col-5" id="dell" onclick="deletarLista()">Deletar Lista</button>
-    </div>
-    </div>`
-    document.getElementById("containerLista").appendChild(div)
-    input.value = "";
-    input.focus();
+    const input = document.getElementById("valorLista");
+    const listaNome = input.value.trim();
+
+    const validationMessageDiv = document.getElementById("nomeListaValidationMessage");
+
+    if (listaNome) {
+        const div = document.createElement("div");
+        div.className = "container border border-primary p-2 fw-bolder justify-content-start " +
+                        "mb-3 col-12 col-sm-6 gap-2 d-flex flex-column";
+
+        const taskName = document.createElement("p");
+        taskName.innerHTML = listaNome;
+        taskName.className = "text-center fs-3";
+
+        const containerButtons = document.createElement("div");
+        const btnAdd = document.createElement("button");
+        const inputValorTarefa = document.createElement("input");
+        inputValorTarefa.className = "form-control";
+
+        btnAdd.className = "btn btn-primary col-6";
+        btnAdd.innerHTML = "Adicionar Tarefa";
+        btnAdd.onclick = () => {
+            addTarefa(div, inputValorTarefa);
+        };
+
+        const btnDel = document.createElement("button");
+        btnDel.className = "btn btn-danger col-6";
+        btnDel.innerHTML = "Deletar Lista";
+        btnDel.onclick = () => {
+            div.remove();
+        };
+
+        containerButtons.className = "d-flex justify-content-around gap-1";
+        containerButtons.append(btnAdd, btnDel);
+
+        const container = document.getElementById("containerLista");
+
+        div.append(taskName);
+        div.append(inputValorTarefa);
+        div.append(containerButtons);
+        container.append(div);
+
+        input.value = "";
+
+        validationMessageDiv.style.display = "none";
+    } else {
+        validationMessageDiv.style.display = "block";
+    }
 }
 
 
-//revisar!!!
-function deletarLista() {
-    let tarefa = document.getElementById("todoList");
-    tarefa.remove();
-}
 
-//revisar!!!
-function deletarTarefa(botao) {
-    let tarefa = document.getElementById("item")
-    tarefa.remove();
-}
 
 function deletarTodasListas(){
     let listas = document.getElementById("containerLista");
