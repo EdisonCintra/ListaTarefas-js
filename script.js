f8ac3b2cdf7a083f382d180990ad5fe121214d44
Edison Cintra de Oliveira Filho <edisoncintra6@gmail.com>
2024-03-20 22:30:19 -0300

    Melhorias design

--- script.js
+++ script.js
@@ -8,32 +8,33 @@ function addLista() {
         if (isListNameUnique(listaNome)){
             const container = document.getElementById("containerLista");
             const div = document.createElement("div");
-            div.className = "lista container border border-primary p-2 fw-bolder justify-content-start mb-3 col-12 col-sm-6 gap-2 d-flex flex-column";
+            div.style.backgroundColor = '#2d2e37';
+            div.className = "lista container rounded-4 border p-2 fw-bolder justify-content-start mb-3 col-12 col-sm-6 gap-2 d-flex flex-column";
 
             const taskName = document.createElement("p");
             taskName.innerHTML = listaNome;
-            taskName.className = "text-center fs-3 text-break";
+            taskName.className = "text-center text-light fs-3 text-break";
 
             const containerButtons = document.createElement("div");
             const btnAdd = document.createElement("button");
             const inputValorTarefa = document.createElement("input");
             inputValorTarefa.className = "form-control";
 
-            btnAdd.className = "btn btn-primary col-6";
+            btnAdd.className = "btn btn-primary col-4 fw-bold";
             btnAdd.innerHTML = "Adicionar Tarefa";
             btnAdd.onclick = () => {
                 addTarefa(div, inputValorTarefa, listaNome);
             };
 
             const btnDel = document.createElement("button");
-            btnDel.className = "btn btn-danger col-6";
+            btnDel.className = "btn btn-danger col-4 fw-bold";
             btnDel.innerHTML = "Deletar Lista";
             btnDel.onclick = () => {
                 div.remove();
                 removeListFromLocalStorage(listaNome);
             };
 
-            containerButtons.className = "d-flex justify-content-around gap-1";
+            containerButtons.className = "d-flex justify-content-around gap-1 mt-3";
             containerButtons.append(btnAdd, btnDel);
 
             div.append(taskName);
@@ -54,6 +55,8 @@ function addLista() {
     }else {
         validationMessageDiv.innerText = "Por favor, adicione um nome para a lista.";
         validationMessageDiv.style.display = "block";
+
+
     }
 
 }
@@ -68,13 +71,15 @@ function addTarefa(div, inputValorTarefa, listaNome) {
             const input = document.createElement("input");
             const p = document.createElement("p");
             const buttonDel = document.createElement("button");
+            const divTESTE = document.createElement("div")
 
-            buttonDel.className = "btn-close";
-            labelTarefa.className = "border-2 border border-dark d-flex flex-row justify-content-center justify-content-evenly align-items-center p-2 ";
+            buttonDel.className = "btn-close bg-danger text-light";
+            labelTarefa.className = "bg-dark mt-3 rounded-4 border-2 border border-dark d-flex justify-content-around flex-row justify-content-center  align-items-center p-2 ";
             input.className = "form-check-input p-3";
             input.setAttribute("type", "checkbox");
-            p.className = "text-break fs-1 mt-2 text-break";
+            p.className = "text-break text-light fs-1 mt-2 text-break";
             p.innerHTML = valorInput;
+            divTESTE.className = "d-flex align-items-center gap-3"
 
             input.addEventListener('change', function() {
                 if (this.checked) {
@@ -89,9 +94,10 @@ function addTarefa(div, inputValorTarefa, listaNome) {
                 removeTaskFromLocalStorage(listaNome, valorInput);
             };
 
-            labelTarefa.appendChild(input);
+            divTESTE.append(input, buttonDel)
             labelTarefa.appendChild(p);
-            labelTarefa.appendChild(buttonDel);
+            labelTarefa.appendChild(divTESTE)
+
 
             div.appendChild(labelTarefa);
             inputValorTarefa.value = "";
@@ -145,27 +151,28 @@ function exibirTudoSalvoNaTela() {
         const tasks = JSON.parse(localStorage.getItem(listaNome) || '[]');
 
         const div = document.createElement("div");
-        div.className = "lista container border border-primary p-2 fw-bolder justify-content-start mb-3 col-12 col-sm-6 gap-2 d-flex flex-column";
+        div.style.backgroundColor = '#2d2e37';
+        div.className = "lista container border rounded-4 p-2 fw-bolder justify-content-start mb-3 col-12 col-sm-6 gap-2 d-flex flex-column";
 
         const taskName = document.createElement("p");
         taskName.innerHTML = listaNome;
-        taskName.className = "text-center fs-3 text-break";
+        taskName.className = "text-center text-light fs-3 text-break";
 
         const inputValorTarefa = document.createElement("input");
         inputValorTarefa.className = "form-control";
 
         const containerButtons = document.createElement("div");
-        containerButtons.className = "d-flex justify-content-around gap-1";
+        containerButtons.className = "d-flex justify-content-around gap-1 mt-3";
 
         const btnAdd = document.createElement("button");
-        btnAdd.className = "btn btn-primary col-6";
+        btnAdd.className = "btn btn-primary col-4 fw-bold";
         btnAdd.innerHTML = "Adicionar Tarefa";
         btnAdd.onclick = () => {
             addTarefa(div, inputValorTarefa, listaNome);
         };
 
         const btnDel = document.createElement("button");
-        btnDel.className = "btn btn-danger col-6";
+        btnDel.className = "btn btn-danger col-4 fw-bold";
         btnDel.innerHTML = "Deletar Lista";
         btnDel.onclick = () => {
             div.remove();
@@ -182,13 +189,16 @@ function exibirTudoSalvoNaTela() {
             const input = document.createElement("input");
             const p = document.createElement("p");
             const buttonDel = document.createElement("button");
+            const  divTESTE = document.createElement("div")
 
-            buttonDel.className = "btn-close";
-            labelTarefa.className = "border-2 border border-dark d-flex flex-row justify-content-center justify-content-evenly align-items-center p-2 ";
+            buttonDel.className = "btn-close bg-danger text-light";
+            labelTarefa.className = "bg-dark mt-3 rounded-4 gap-5 border-2 border border-dark d-flex justify-content-around flex-row justify-content-center  align-items-center p-2 ";
             input.className = "form-check-input p-3";
             input.setAttribute("type", "checkbox");
-            p.className = "text-break fs-1 mt-2 text-break";
+            p.className = "text-break text-light fs-1 mt-2 text-break";
             p.innerHTML = task.name;
+            divTESTE.className = "d-flex align-items-center gap-3"
+
 
             input.addEventListener('change', function() {
                 // Verifica se o checkbox está marcado
@@ -204,9 +214,10 @@ function exibirTudoSalvoNaTela() {
                 removeTaskFromLocalStorage(listaNome, task.name);
             };
 
-            labelTarefa.appendChild(input);
+
+            divTESTE.append(input, buttonDel)
             labelTarefa.appendChild(p);
-            labelTarefa.appendChild(buttonDel);
+            labelTarefa.appendChild(divTESTE)
 
             div.appendChild(labelTarefa);
         });
